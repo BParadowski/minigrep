f851ae163d883fcfee86fad0f46683f5c6d815e4
Bartosz Paradowski <bartoszparadowski01@gmail.com>
2024-07-16 21:45:54 +0200

    Add reading files to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,4 +7,9 @@ fn main() {
     let path_to_file = &args[2];
 
     println!("Looking for {query} in {path_to_file}");
+
+    let contents = fs::read_to_string(path_to_file).expect("File should exist!");
+
+    println!("{contents}");
+
 }
