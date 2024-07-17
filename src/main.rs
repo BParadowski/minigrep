7fb1e6e03a060ee4bfdab89488a4768f8c38ee86
Bartosz Paradowski <bartoszparadowski01@gmail.com>
2024-07-17 19:10:25 +0200

    Implement error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -9,11 +10,10 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Looking for {} in {}", config.query, config.path);
-
-    let contents = fs::read_to_string(config.path).expect("File should exist!");
-
-    println!("{contents}");
+    if let Err(e) = run(config) {
+        println!("Encountered an error: {:?}", e);
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -24,11 +24,19 @@ struct Config {
 impl Config {
     fn build(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
-           return  Err("Fewer than 2 arguments provided.");
+            return Err("Fewer than 2 arguments provided.");
         }
         let query = args[1].clone();
         let path = args[2].clone();
 
         Ok(Config { query, path })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    println!("Looking for {} in {}", config.query, config.path);
+    let contents = fs::read_to_string(config.path)?;
+    println!("{contents}");
+
+    Ok(())
 }
