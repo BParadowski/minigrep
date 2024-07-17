625c1b0765eb85f4502aa834ac4ee3b3a49ebce4
Bartosz Paradowski <bartoszparadowski01@gmail.com>
2024-07-17 22:27:36 +0200

    Add stderr output

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Encountered an error: {err}");
+        eprintln!("Encountered an error: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Encountered an error: {:?}", e);
+        eprintln!("Encountered an error: {:?}", e);
         process::exit(1);
     }
 }
